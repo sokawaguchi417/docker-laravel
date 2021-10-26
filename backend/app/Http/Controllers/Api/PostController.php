1f19ab0d205976b422d748d22a3d304c30fb5dd8
そんぐ <s.kawaguchi@gftd.works>
2021-10-26 16:50:59 +0900

    laravel側でindex()でpost一覧の取得を実装

---
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    // post一覧表示
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json($posts, 200);
+    }
+}
