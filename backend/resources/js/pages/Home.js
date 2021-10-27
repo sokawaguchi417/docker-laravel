ef3d9294ded2f9ace9d2b4d1175bac6c5f5f9f9f
そんぐ <s.kawaguchi@gftd.works>
2021-10-27 10:36:42 +0900

    DBのpostsデータをフロント側で取得しステート(posts)にセット

--- backend/resources/js/pages/Home.js
+++ backend/resources/js/pages/Home.js
@@ -1,14 +1,17 @@
-import React　from 'react';
+import React, { useState, useEffect }　from 'react';
 import { Button, Card } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import MainTable from '../components/MainTable';
+import axios from 'axios';
+import { grep } from 'jquery';
+
 
 //スタイルの定義
 const useStyles = makeStyles((theme) => createStyles({
-    card: {
-        margin: theme.spacing(5),
-        padding: theme.spacing(3),
-    },
+  card: {
+    margin: theme.spacing(5),
+    padding: theme.spacing(3),
+  },
 }));
 
 //ヘッダーのコンテンツ用の配列定義
@@ -27,14 +30,40 @@ let rows = [
     editBtn: <Button color="secondary" variant="contained">編集</Button>,
     deleteBtn: <Button color="primary" variant="contained">完了</Button>,
   }
- ]
+]
 
 function Home() {
-    //定義したスタイルを利用するための設定
-    const classes = useStyles();
+  
+  
+  //定義したスタイルを利用するための設定
+  const classes = useStyles(); 
+  
+  //postsの状態を管理する
+  const [posts, setPosts] = useState([]);
+  
+   //画面に到着したらgetPostsDataを呼ぶ
+  useEffect(() => {
+    getPostsData();
+    },[])
 
-    return (
-        <div className="container">
+
+  const getPostsData = () => {
+    // バックエンドからpostsの一覧を取得する処理
+    axios
+    .get('/api/posts')
+    .then(response => {
+        setPosts(response.data);     //バックエンドから返ってきたデータでpostsを更新する
+        console.log(response.data);　//取得データ確認用のconsole.log()
+    })
+    .catch(() => {
+        console.log('通信に失敗しました');
+    });   
+  }
+  
+  
+  
+  return (
+    <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-10">
                     <div className="card">
